c64374fa6d05563ccbf5b5399837e148c49fa1a0
Štěpán Hofmann <111199925+neonek26@users.noreply.github.com>
2023-04-14 18:06:14 +0200

    Add files via upload

---
+++ bogosort.js
@@ -0,0 +1,24 @@
+function bogosort(arr) {
+    function isSorted(arr) {
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    function shuffle(arr) {
+      for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+      return arr;
+    }
+  
+    while (!isSorted(arr)) {
+      arr = shuffle(arr);
+    }
+    return arr;
+  }
+  
